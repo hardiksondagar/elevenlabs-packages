d8c32d0578fab8b52ff1d37712f8a35fd864f16d
Hardik Sondagar <hardikmsondagar@gmail.com>
2025-09-01 19:21:12 +0530

    Update version.ts

--- packages/react/src/version.ts
+++ packages/react/src/version.ts
@@ -1,6 +1,2 @@
 // This file is auto-generated during build
-<<<<<<< HEAD
 export const PACKAGE_VERSION = "0.6.0";
-=======
-export const PACKAGE_VERSION = "0.5.0";
->>>>>>> 1fb3bb0 (Update package version)
